b1063a15f0994331e5ed08e9190874dcace3a68f
Jeffrs123 <rs.mensur@gmail.com>
2021-04-20 20:03:16 -0300

    corpo dos métodos para análise de cada dado e geração dos relatórios

--- src/main/java/com/accenture/dao/BusinessProcess.java
+++ src/main/java/com/accenture/dao/BusinessProcess.java
@@ -13,26 +13,32 @@ public class BusinessProcess {
 	
 	List<Data> listaBruto = new ArrayList<Data>();
 	
+	private static String FULL_PATH_IN = "";
+	private static String FULL_PATH_OUT = "";
+	private static String FULL_PATH_LOGGER = "";
+	
 	public static void startProcess(String path) throws IOException {		
-		lerDiretorio(path);
-		
-
-//		List<String> listaParaAnalisar = Files.readAllLines(Path.of(fullPath));
-//		processFile(listaParaAnalisar);
+		setPaths(path);
+		lerDiretorio();
+	}
+	
+	private static void setPaths(String path) {
+		FULL_PATH_IN = path + "\\HOMEPATH\\data\\in";
+		FULL_PATH_OUT = path + "\\HOMEPATH\\data\\out";
+		FULL_PATH_LOGGER = path + "\\HOMEPATH\\data\\logger";
 	}
 	
 	private static List<String> processedFiles = new ArrayList<String>();
 	
-	private static void lerDiretorio(String path) {
-		String fullPath = path + "\\HOMEPATH\\data\\in";
+	private static void lerDiretorio() {
 		File arquivos[];
-		File diretorio = new File(fullPath);
+		File diretorio = new File(FULL_PATH_IN);
 		arquivos = diretorio.listFiles();
 		
 		for(int i = 0; i < arquivos.length; i++){
 			/** Caso o arquivo não tenha sido processado, iremos processar */
 			if(!processedFiles.contains(arquivos[i].getName())) {
-				processFile(fullPath, arquivos[i].getName());
+				processFile(arquivos[i].getName());
 			}
 		}
 	}
@@ -43,11 +49,11 @@ public class BusinessProcess {
 	 * @param path Caminho de onde o arquivo se encontra
 	 * @param fileName Nome do Arquivo
 	 */
-	private static void processFile(String path, String fileName) {
+	private static void processFile(String fileName) {
 		try {
 			System.out.println("Processando o arquivo: " + fileName);
 			processedFiles.add(fileName);
-			String fullPath = path + "\\" + fileName;
+			String fullPath = FULL_PATH_IN + "\\" + fileName;
 			List<String> linhasDadosCompra = Files.readAllLines(Path.of(fullPath));		
 			for (String linha : linhasDadosCompra) {
 				processData(linha);
@@ -64,18 +70,17 @@ public class BusinessProcess {
 		try {
 			switch(idData) {
 				case 001:{
-					System.out.println("sou um vendedor!");
+					sellerLogic(colunas);
 					break;
 				}
 				case 002:{
-					System.out.println("sou um cliente!");
+					customerLogic(colunas);
 					break;
 				}
 				case 003:{
-					System.out.println("sou uma transação!");
+					transactionLogic(colunas);
 					break;
 				}
-				
 				default:{
 					System.err.println("Arquivo sem identificador válido");
 					break;
@@ -84,6 +89,26 @@ public class BusinessProcess {
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
-		
+	}
+	
+	private static void sellerLogic(String[] seller) {
+		System.out.println("sou um vendedor!");		
+	}
+	
+	private static void customerLogic(String[] customer) {
+		System.out.println("sou um cliente!");		
+	}
+	
+	private static void transactionLogic(String[] transaction) {
+		System.out.println("sou uma transação!");		
+	}
+	
+	
+	private static void generateRalatory() {
+		System.out.println("Gerado relatório");	
+	}
+	
+	private static void generateLogger() {
+		System.out.println("Gerado relatório LOGGER_ERRO");
 	}
 }
